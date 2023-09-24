b379aa432ef14fdbafa4ee01dd4eb9b148508b0b
Darko Skulikj <ds7363@nyu.edu>
2023-09-24 23:30:22 +0400

    update

--- java.js
+++ java.js
@@ -26,3 +26,39 @@ anchorList.forEach(link => {
         });
     }
 });
+
+var item = document.getElementById("Aboutpicture1");
+item.addEventListener("mouseover", func1, false);
+
+function func1()
+{  
+   var item = document.getElementById("abouttext1");
+   item.setAttribute("style", "visibility:visible")
+}
+
+var item = document.getElementById("Aboutpicture2");
+item.addEventListener("mouseover", func2, false);
+
+function func2()
+{  
+   var item = document.getElementById("abouttext2");
+   item.setAttribute("style", "visibility:visible")
+}
+
+var item = document.getElementById("Aboutpicture3");
+item.addEventListener("mouseover", func3, false);
+
+function func3()
+{  
+   var item = document.getElementById("abouttext3");
+   item.setAttribute("style", "visibility:visible")
+}
+
+var item = document.getElementById("Aboutpicture4");
+item.addEventListener("mouseover", func4, false);
+
+function func4()
+{  
+   var item = document.getElementById("abouttext4");
+   item.setAttribute("style", "visibility:visible")
+}
